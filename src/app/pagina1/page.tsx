1067a6bc8e92047a5c9fdf802e66bb5caad17c70
al3xpisani <alexandre.pisani.ant@gmail.com>
2024-01-31 14:34:15 -0300

    react ts nextjs setup

---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function Pagina1() {
+    return (
+        <div>
+            Page 1
+            <Button variant="contained" color="primary">aaa</Button>
+        </div>
+    )
+}
+
+export default Pagina1
